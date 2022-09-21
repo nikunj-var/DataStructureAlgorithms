611901f939587db8a1cd5c27a5e0ea1ec3510b01
Nikunj <nikunjvar2002@gmail.com>
2022-09-20 22:09:03 -0700

    adding more files

---
+++ dp_CP/cutitnosegments.cpp
@@ -0,0 +1,44 @@
+//using recursion
+#include<limits.h>
+int solve(int n,int x,int y,int z){
+    if(n == 0)
+        return 0;
+    if(n < 0)
+        return INT_MIN;
+    
+    int a=solve(n-x,x,y,z)+1;
+    int b=solve(n-y,x,y,z)+1;
+    int c=solve(n-z,x,y,z)+1;
+    return max(a,max(b,c));
+    
+}
+int cutSegments(int n, int x, int y, int z) {
+	int ans=solve(n,x,y,z);
+    if(ans < 0)
+        return 0;
+    return ans;
+}
+
+//top-down approach
+#include<limits.h>
+int solve(int n,int x,int y,int z,vector<int>& dp){
+   if(n == 0)
+       return 0;
+    if(n < 0)
+        return INT_MIN;
+    if(dp[n] != -1)
+        return dp[n];
+    int a=solve(n-x,x,y,z,dp)+1;
+    int b=solve(n-y,x,y,z,dp)+1;
+    int c=solve(n-z,x,y,z,dp)+1;
+    dp[n]=max(a,max(b,c));
+    return dp[n];
+}
+int cutSegments(int n, int x, int y, int z) {
+    vector<int> dp(n+1,-1);
+	int ans=solve(n,x,y,z,dp);
+    if(ans < 0)
+        return 0;
+    return ans;
+}
+
---
+++ dp_CP/houserobber.cpp
@@ -0,0 +1,17 @@
+//space-optimisation
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+         int prev1=nums[0];
+         int prev2=0;
+        for(int i=1;i<nums.size();i++){
+            int incl=prev2+nums[i];
+            int excl=prev1+0;
+            
+            int ans=max(incl,excl);
+            prev2=prev1;
+            prev1=ans;
+        }
+        return prev1;
+    }
+};
