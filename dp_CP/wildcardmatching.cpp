5474850f914921719e1145398b782c8cacc2aa6d
Nikunj <nikunjvar2002@gmail.com>
2022-10-12 07:29:56 -0700

    adding more files

---
+++ dp_CP/wildcardmatching.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool solve(string s, string p,int i,int j){
+        if(i < 0 && j < 0)
+            return true;
+        if(i >= 0 && j < 0)    
+            return false;
+        if(i < 0 && j >= 0){
+            for(int k=0;k<=j;k++){
+                if(p[k] != '*')
+                return false;
+            }
+            return true;
+        }
+
+        if(s[i] == p[j] || p[j] == '?')
+            return solve(s,p,i-1,j-1);
+        else if(p[j] == '*'){
+            return (solve(s,p,i-1,j) || solve(s,p,i,j-1));
+        }    
+        else
+            return false;
+    }
+    bool isMatch(string s, string p) {
+        return solve(s,p,s.length()-1,p.length()-1);
+    }
+};
+
+//using recursion and memoization
+class Solution {
+public:
+    bool solve(string s, string p,int i,int j,vector<vector<int>>& dp){
+        if(i < 0 && j < 0)
+            return true;
+        if(i >= 0 && j < 0)    
+            return false;
+        if(i < 0 && j >= 0){
+            for(int k=0;k<=j;k++){
+                if(p[k] != '*')
+                return false;
+            }
+            return true;
+        }
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        if(s[i] == p[j] || p[j] == '?')
+            return dp[i][j]=solve(s,p,i-1,j-1,dp);
+        else if(p[j] == '*'){
+            return dp[i][j]= (solve(s,p,i-1,j,dp) || solve(s,p,i,j-1,dp));
+        }    
+        else
+            return false;
+    }
+    bool isMatch(string s, string p) {
+        vector<vector<int>> dp(s.length(),vector<int>(p.length(),-1));
+        return solve(s,p,s.length()-1,p.length()-1,dp);
+    }
+};
