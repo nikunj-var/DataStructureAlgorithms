9ea0f6ab130e6854381ff7493cb58b0c1e931ff0
nikunj-var <nikunjvar2002@gmail.com>
2022-11-15 20:09:55 +0530

    adding more files

--- array/array_lb/medianoftwoarrayofsamesize.cpp
+++ array/array_lb/medianoftwoarrayofsamesize.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
  
 double getMedian(int a1[],int a2[],int n){
---
+++ array/array_lb/minimunswaptosortarray.cpp
@@ -0,0 +1,25 @@
+#include<vector>
+using namespace std;
+
+class Solution 
+{
+    public:
+    //Function to find the minimum number of swaps required to sort the array. 
+	int minSwaps(vector<int>&nums)
+	{       int n=nums.size();
+	        vector<pair<int,int>> v(n);
+	        for(int i=0;i<nums.size();i++){
+	            v[i]={nums[i],i};
+	        }
+	        sort(v.begin(),v.end());
+	        int ans=0;
+	        for(int i=0;i<n;i++){
+	            if(v[i].second != i){
+	                ans++;
+	                swap(v[i],v[v[i].second]);
+	                i--;
+	            }
+	        }
+	   return ans;
+	}
+};
